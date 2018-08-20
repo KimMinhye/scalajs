7dc4da60bd6f7f72d472fa810a2e3f60f10663e4
Kim Minhye <mine517@nate.com>
2018-08-20 16:35:58 +0900

    chap16_1

--- src/main/scala/chap16/Cahp16.scala
+++ src/main/scala/chap16/Cahp16.scala
@@ -3,10 +3,23 @@ package scalaJSExample
 import scala.scalajs.js.annotation.{JSExportTopLevel, JSExport}
 import scala.scalajs.js
 import js.Dynamic.{global => g}
-import org.scalajs.dom
-import org.scalajs.dom.html
-import org.scalajs.dom.raw.{Event, MouseEvent, KeyboardEvent}
-import scalatags.JsDom.all._
+import org.scalajs.dom.{html}
+import org.scalajs.dom.raw.{Event, XMLHttpRequest}
 
 @JSExportTopLevel("Chap16")
-object Chap16 {}
+object Chap16 {
+  @JSExport
+  def onreadystatechange(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.onreadystatechange = (_: Event) => {
+      if (req.readyState.toInt == 4) {
+        if (req.status == 200) {
+          g.document.getElementById("view").innerHTML = req.responseText
+        }
+      }
+    }
+    req.open("GET", "../data/data.txt")
+    req.send()
+  }
+}
