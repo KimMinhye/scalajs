89248c879dc87b55615a0b8595dfbcc55aae8c84
Kim Minhye <mine517@nate.com>
2018-08-20 16:34:39 +0900

    chap16_1

---
+++ src/main/scala/chap16/Cahp16.scala
@@ -0,0 +1,12 @@
+package scalaJSExample
+
+import scala.scalajs.js.annotation.{JSExportTopLevel, JSExport}
+import scala.scalajs.js
+import js.Dynamic.{global => g}
+import org.scalajs.dom
+import org.scalajs.dom.html
+import org.scalajs.dom.raw.{Event, MouseEvent, KeyboardEvent}
+import scalatags.JsDom.all._
+
+@JSExportTopLevel("Chap16")
+object Chap16 {}
