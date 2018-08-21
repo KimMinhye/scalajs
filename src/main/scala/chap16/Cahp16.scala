e80a64cd4e3b05519399295a55f44a9803d55698
Kim Minhye <mine517@nate.com>
2018-08-21 15:45:44 +0900

    Chapter16 postMessage

--- src/main/scala/chap16/Cahp16.scala
+++ src/main/scala/chap16/Cahp16.scala
@@ -3,8 +3,10 @@ package scalaJSExample
 import scala.scalajs.js.annotation.{JSExportTopLevel, JSExport}
 import scala.scalajs.js
 import js.Dynamic.{global => g}
-import org.scalajs.dom.{html}
-import org.scalajs.dom.raw.{Event, XMLHttpRequest}
+import scala.scalajs.js.JSON
+import org.scalajs.dom
+import org.scalajs.dom.html
+import org.scalajs.dom.raw.{Event, XMLHttpRequest, MessageEvent}
 
 @JSExportTopLevel("Chap16")
 object Chap16 {
@@ -21,5 +23,48 @@ object Chap16 {
     }
     req.open("GET", "../data/data.txt")
     req.send()
+  }
+
+  @JSExport
+  def responseType(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("load", (_: Event) => {
+      val jsonObj = req.response
+      g.console.log(jsonObj)
+    }, false)
+    req.responseType = "json"
+    req.open("GET", "../data/data.json")
+    req.send()
+  }
+
+  @JSExport
+  def jsonp(): Unit = {
+    val url: String = "../data/jsonp.js"
+    val script      = g.document.createElement("script").asInstanceOf[html.Script]
+    script.setAttribute("src", url)
+    // g.document.getElementsByTagName("head")(0).appendChild(script)
+    dom.document.getElementsByTagName("head")(0).appendChild(script)
+  }
+
+  @JSExport
+  def postMessage(): Unit = {
+    val frameWindow  = g.document.getElementById("frame").contentWindow
+    val message      = g.document.getElementById("message")
+    val button       = g.document.getElementById("button")
+    val targetOrigin = "/"
+    button.addEventListener("click", (_: Event) => {
+      frameWindow.postMessage(message.value, targetOrigin)
+    }, false)
+  }
+
+  @JSExport
+  def otherDomain(): Unit = {
+    val display = g.document.getElementById("dispaly").asInstanceOf[html.Div]
+    g.window.addEventListener("message", (e: MessageEvent) => {
+      val sourceOrigin = g.location.origin
+      if (e.origin == sourceOrigin) return
+      display.textContent = e.data.toString
+    }, false)
   }
 }
